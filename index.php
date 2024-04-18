87312f7e8dbe36c261d4cc28f89e0748811710e2
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-04-18 18:49:05 +0530

    feat:base of sign up box

--- index.php
+++ index.php
@@ -9,23 +9,38 @@
 </head>
 
 <body class="scroll-smooth font-Poppins">
-    <main class="text-lg">
+    <main class="text-lg border">
         <div class="border">
             <img src="./assets/icons/person_icon.svg" alt="person" class="opacity-50 inline w-8 h-10 mr-2">
             <input type="text" name="f_name" id="full_name" placeholder="Enter Your Full Name" class=" align-middle">
         </div>
-        <input type="number" name="u_age" id="u_age" placeholder="Age">
-        <input type="date" name="u_dob" id="u_dob" placeholder="Date Of Birth">
+        <div class="border">
+            <input type="number" name="u_age" id="u_age" placeholder="Age">
+        </div>
+        <div class="border">
+            <label>DOB:</label>
+            <input type="date" name="u_dob" id="u_dob">
+        </div>
         <div class="border">
             <img src="./assets/icons/mail_icon.svg" alt="mail" class="opacity-50 inline w-8 h-9 mr-2">
             <input type="email" name="u_mail" id="u_mail" placeholder="E-mail Id" class="align-middle">
         </div>
-        <input type="number" name="u_num" id="u_num" placeholder="Phone Number">
-        <input type="text" name="u_org" id="u_org" placeholder="Organisation">
-        <input type="text" name="u_username" id="u_username" placeholder="Username">
-        <input type="password" name="u_password" id="u_password" placeholder="Password">
-        <label>Upload your Photo:</label>
-        <input type="file" name="u_img" id="u_img">
+        <div class="border">
+            <input type="number" name="u_num" id="u_num" placeholder="Phone Number">
+        </div>
+        <div class="border">
+            <input type="text" name="u_org" id="u_org" placeholder="Organisation">
+        </div>
+        <div class="border">
+            <input type="text" name="u_username" id="u_username" placeholder="Username">
+        </div>
+        <div class="border">
+            <input type="password" name="u_password" id="u_password" placeholder="Password">
+        </div>
+        <div class="border">
+            <label>Upload your Photo:</label>
+            <input type="file" name="u_img" id="u_img">
+        </div>
     </main>
 </body>
 
