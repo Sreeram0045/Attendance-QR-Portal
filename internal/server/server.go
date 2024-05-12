16fa2c264b98b462a4f6c47075dba098e7baabe7
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-05-12 13:31:38 +0530

    feat:Added some comments to understand the code better

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,17 +9,24 @@ import (
 )
 
 func checkingPathForServer() string {
+	// created two variables to check/compare the path
 	comparison := "/Attendance-QR-Portal"
 	lastindex := "/pages/"
+	// created two variables one will get the current working directory and other will recieve the error
 	rootDir, err := os.Getwd()
+	// error handling
 	if err != nil {
 		fmt.Println("Error getting current working directory:", err)
 		return ""
 	}
+	// checks if the current working directory contains the compared path
 	if strings.Contains(rootDir, comparison) {
+		// The current working directory is split into before and after the comparison variable
 		slices := strings.SplitAfter(rootDir, comparison)
+		// The first part of the string is joined with the lastindex varible in-order to get the correct path to serve
 		return (strings.Join([]string{slices[0], lastindex}, ""))
 	}
+	// if the variable is not present as a part of the working directory an empty string is returned which will obviously result in error
 	return ""
 }
 
