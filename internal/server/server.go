9d79d733427e3bf98bfa26ecaa496046c332722b
“Sreeram0045” <sreeramjmenon10@gmail.com>
2024-05-12 13:22:35 +0530

    fix:Fixed the path issue; from wherever the main.go file is executed in the root directory the server will host the pages sub-directory without any conflicts

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,10 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
+func checkingPathForServer() string {
+	comparison := "/Attendance-QR-Portal"
+	lastindex := "/pages/"
+	rootDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current working directory:", err)
+		return ""
+	}
+	if strings.Contains(rootDir, comparison) {
+		slices := strings.SplitAfter(rootDir, comparison)
+		return (strings.Join([]string{slices[0], lastindex}, ""))
+	}
+	return ""
+}
+
 func ServerStart() {
-	http.Handle("/", http.FileServer(http.Dir("../../pages/")))
+	path := checkingPathForServer()
+	http.Handle("/", http.FileServer(http.Dir(path)))
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
